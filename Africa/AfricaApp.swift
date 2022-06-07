aee2afe899c25152475a553af9b02c995f1663f9
Pranav Nithesh J <pranavnithesh@gmail.com>
2022-06-07 22:57:59 +0530

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by M1089943 on 07/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
